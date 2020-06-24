643a2fbcd0144652958369fc7d9338f02138a48c
booldook <booldook@gmail.com>
2020-06-24 15:25:21 +0900

    bootstrap 정리

--- js/04.bootstrap.js
+++ js/04.bootstrap.js
@@ -18,4 +18,13 @@ $(".tab-wrap").find(".nav-link").click(function(){
 	$(".tab-cont").eq($(this).index()).removeClass('d-none');
 });
 
+$('[data-toggle="tooltip"]').tooltip();
 $('.popover-bt').popover({ container: 'body', offset: 200 });
+
+$(".bt-photoshop").click(function(){
+	$(".photoshop").find(".progress-bar").css("width", "82%");
+});
+
+$('#btToast').click(function(){
+	$('#alertToast').toast('show');
+});
