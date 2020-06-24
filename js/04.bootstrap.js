42b77bb6ae8c8e68eef2dc50094eb9d57b3a619a
booldook <booldook@gmail.com>
2020-06-24 12:19:52 +0900

    04.bootstrap

--- js/04.bootstrap.js
+++ js/04.bootstrap.js
@@ -3,3 +3,17 @@ $(".musics > .music").click(function(){
 	$(this).addClass("active");
 });
 $(".musics > .music").eq(0).trigger("click");
+
+$(".bt-modal").click(function(){
+	$("#modal1").modal('show');
+});
+$(".bt-close").click(function(){
+	$("#modal1").modal('hide');
+});
+
+$(".tab-wrap").find(".nav-link").click(function(){
+	$(this).siblings().removeClass('active');
+	$(this).addClass('active');
+	$(".tab-cont").addClass('d-none');
+	$(".tab-cont").eq($(this).index()).removeClass('d-none');
+});
